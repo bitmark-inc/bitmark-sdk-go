7ce1f4258189686472280c455deefd23f0182baf
Jim Yeh <jim@bitmark.com>
2019-01-22 12:59:16 +0800

    Add share info into the transaction object

--- tx/structs.go
+++ tx/structs.go
@@ -5,13 +5,16 @@ import (
 )
 
 type Tx struct {
-	Id          string `json:"id"`
-	BitmarkId   string `json:"bitmark_id"`
-	AssetId     string `json:"asset_id"`
-	Asset       *asset.Asset
-	Owner       string `json:"owner"`
-	Status      string `json:"status"`
-	BlockNumber int    `json:"block_number"`
-	Sequence    int    `json:"offset"`
-	PreviousId  string `json:"previous_id"`
+	Id           string `json:"id"`
+	BitmarkId    string `json:"bitmark_id"`
+	AssetId      string `json:"asset_id"`
+	Asset        *asset.Asset
+	Owner        string `json:"owner"`
+	Status       string `json:"status"`
+	BlockNumber  int    `json:"block_number"`
+	Sequence     int    `json:"offset"`
+	PreviousId   string `json:"previous_id"`
+	Confirmation uint64 `json:"confirmation"`
+
+	ShareInfo map[string]interface{} `json:"share_info,omitempty"`
 }
