5093d8b07d37a31405bbfae973d0d38bfdbf720f
araujobsd <msa@bitmark.com>
2019-07-19 15:46:24 +0800

    - Add ISC LICENSE and SPDX tag all over the project.

--- vendor/golang.org/x/crypto/curve25519/doc.go
+++ vendor/golang.org/x/crypto/curve25519/doc.go
@@ -1,3 +1,7 @@
+// SPDX-License-Identifier: ISC
+// Copyright (c) 2014-2019 Bitmark Inc.
+// Use of this source code is governed by an ISC
+// license that can be found in the LICENSE file.
 // Copyright 2012 The Go Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style
 // license that can be found in the LICENSE file.
