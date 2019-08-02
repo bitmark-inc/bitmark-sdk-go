89ab3b358fdafc1af49b3df4c00fbdccda65bfea
Linzy Hu <linzy.hu@gmail.com>
2019-08-02 16:36:20 +0800

    fix response not signed error message

--- bitmark/client.go
+++ bitmark/client.go
@@ -91,7 +91,7 @@ func Offer(params *OfferParams) error {
 
 func Respond(params *ResponseParams) (string, error) {
 	if params.auth.Get("signature") == "" {
-		return "", errors.New("response not signed by sender")
+		return "", errors.New("response not signed")
 	}
 
 	client := sdk.GetAPIClient()
